c2a2266bbb8e4411fd6dfd039624a30227811455
Karolina Solanska <karolina@solanska.cz>
2017-11-14 22:27:10 +0100

    major changes in way it handles things

--- js/simpletripscript.js
+++ js/simpletripscript.js
@@ -12,7 +12,18 @@ var loggedIn = localStorage.getItem("loggedIn");
 if (!loggedIn){
     loggedIn = false;
 }
+
+var where = localStorage.getItem("where");
+if (!where){
+    where = "praha";
+}
+
+var whereItinerary = localStorage.getItem("whereItinerary");
+if (!whereItinerary){
+    whereItinerary = "praha";
+}
 var wentRight = false;
+
 $(document).ready(function () {
     $("#menui0").click(function (event) {
         showMockup('index.html');
@@ -40,7 +51,7 @@ $(document).ready(function () {
     });
 
     $("#img-landing-page").click(function (event) {
-        showMockup('ddetalakce.html');
+        showMockup('detalakce.html');
         event.stopPropagation();
         event.preventDefault();
 
@@ -61,24 +72,18 @@ $(document).ready(function () {
     });
 
     $("#btn-1").click(function (event) {
-        showMockup('detalakce.html');
-        event.stopPropagation();
-        event.preventDefault();
-
+        showMockup('detailitinerary.html');
     });
 
     $("#btn-2").click(function (event) {
-        showMockup('detalakce.html');
-        event.stopPropagation();
-        event.preventDefault();
-
+        showMockup('detailitinerary.html');
     });
 
     $("#btn-3").click(function (event) {
         showMockup('detalakce.html');
-        event.stopPropagation();
-        event.preventDefault();
-
+    });
+    $("#btn-4").click(function (event) {
+        showMockup('detalakce.html');
     });
 
     $("#passwd-field").click(function (event) {
@@ -139,25 +144,76 @@ $(document).ready(function () {
     })
 
     $("#arrow-icon2").click(function (event) {
-
-        dataDetail()
+        $("#slide1").toggleClass("invisible");
+        $("#slide4").toggleClass("invisible");
     })
 
+    $("#arrow-icon1").click(function (event) {
+        $("#slide1").toggleClass("invisible");
+        $("#slide4").toggleClass("invisible");
+    })
+
+    $("#arrow-icon3").click(function (event) {
+        $("#slider1").toggleClass("invisible");
+        $("#slider4").toggleClass("invisible");
+    })
+
+    $("#arrow-icon4").click(function (event) {
+        $("#slider1").toggleClass("invisible");
+        $("#slider4").toggleClass("invisible");
+    })
+
+    itineraryDetail(whereItinerary);
+    dataDetail(where);
    logIn();
 
 });
 
-function dataDetail(where) {
-    if(where === "praha"){
-        $("#akce-title").text("Staroměstská radnice");
-        $("#main_image_detail").attr("src","staromestska-radnice.jpg");
-    }
-    if(where === "morava"){
-        $("#akce-title").text("Staroměstská radnice");
-    }
-    if(where === "morava"){
-        $("#akce-title").text("Staroměstská radnice");
-    }
+function dataDetail(whe) {
+    where = whe;
+    localStorage.setItem("where", where);
+    $(document).ready(function () {
+        if (where === "praha") {
+            $("#akce-title").text("Staroměstská radnice");
+            $("#main_image_detail").attr("src", "images/staromestska-radnice.jpg");
+            $("#image1_detail").attr("src", "images/staromestska-radnice-2.jpg");
+            $("#image2_detail").attr("src", "images/staromestska-radnice-3.jpg");
+            $("#image3_detail").attr("src", "images/staromestska_radnice_vanoce.jpg");
+        }
+        else if (where === "morava") {
+            $("#akce-title").text("Staroměstská radnice");
+        }
+        else if (where === "krumlov") {
+            $("#akce-title").text("Staroměstská radnice");
+        }
+        else if (where === "zvirata-praha") {
+            $("#akce-title").text("V Praze za zvířátky");
+        }
+    })
+
+}
+
+function itineraryDetail(whe) {
+    whereItinerary = whe;
+    localStorage.setItem("whereItinerary", whereItinerary);
+    $(document).ready(function () {
+        if (where === "praha") {
+            $("#akce-title").text("Staroměstská radnice");
+            $("#main_image_detail").attr("src", "images/staromestska-radnice.jpg");
+            $("#image1_detail").attr("src", "images/staromestska-radnice-2.jpg");
+            $("#image2_detail").attr("src", "images/staromestska-radnice-3.jpg");
+            $("#image3_detail").attr("src", "images/staromestska_radnice_vanoce.jpg");
+        }
+        else if (where === "morava") {
+            $("#akce-title").text("Staroměstská radnice");
+        }
+        else if (where === "krumlov") {
+            $("#akce-title").text("Staroměstská radnice");
+        }
+        else if (where === "zvirata-praha") {
+            $("#akce-title").text("V Praze za zvířátky");
+        }
+    })
 
 }
 
@@ -167,6 +223,7 @@ function logIn() {
         $("#sign-in-form").toggleClass("invisible");
         $("#sign-out-form").toggleClass("invisible");
         $("#menui2").toggleClass("invisible");
+        $("#recommended").toggleClass("invisible");
     }
 }
 
@@ -175,5 +232,28 @@ function logOut() {
         $("#sign-in-form").toggleClass("invisible");
         $("#sign-out-form").toggleClass("invisible");
         $("#menui2").toggleClass("invisible");
+        $("#recommended").toggleClass("invisible");
     }
+}
+
+function dropDownMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function refresh() {
+    location.reload();
 }
